7664931d383a64d0493793a41c277e5e77162651
Ayrton Neves <ayrton.leandro@gmail.com>
2020-07-05 22:40:34 -0300

    parte III - ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
+from config import Config
 
-# No Python, um subdiretório que inclui um arquivo __init__.py é considerado um pacote 
-# e pode ser importado. 
-# Quando você importa um pacote, o __init__.py executa e define quais símbolos o pacote 
-# expõe ao mundo externo.
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 from app import routes
